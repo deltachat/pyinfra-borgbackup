ab9b196e64de7a4b60b4ecd89365fdf88bf41f8c
missytake <missytake@systemli.org>
2025-11-17 11:26:22 +0100

    fix backup-pre.py exit code handling

--- pyinfra_borgbackup/backup-pre.py
+++ pyinfra_borgbackup/backup-pre.py
@@ -3,6 +3,12 @@
 import argparse
 import os
 
+
+def cmd(command: str) -> int:
+    """Run a command and return its exit code. """
+    return os.waitstatus_to_exitcode(os.system(command))
+
+
 services = {
     "isolationbot": "isolationbot.service",
     "root": "docker.service",
@@ -19,9 +25,9 @@ args = parser.parse_args()
 
 for user in services:
     if user == "root":
-        returncode = os.system(f"systemctl {args.command} {services[user]}")
+        returncode = cmd(f"systemctl {args.command} {services[user]}")
     else:
-        returncode = os.system(
+        returncode = cmd(
             f"su -l {user} -c 'systemctl --user {args.command} {services[user]}'"
         )
     if returncode != 0:
